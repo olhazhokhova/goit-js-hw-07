c101d758f852fe1dc00d1424ba341296ff6c2ca7
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-20 19:19:07 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryListMarkup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryListMarkup);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
