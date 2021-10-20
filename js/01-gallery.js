8594648b16df6d62bfce2a59ed7d485b6ecd84d2
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-20 18:41:38 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryListMarkup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryListMarkup);
+gallery.addEventListener("click", showModal);
+
+function showModal(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("img").onclick = instance.close;
+      },
+    }
+  );
+
+  instance.show();
+
+  window.addEventListener('keydown', e => e.code === "Escape" && instance.close());
+}
